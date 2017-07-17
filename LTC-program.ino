6f3bfa64dbc6b64a7dbbb69d8c79245dd93a0c58
Sean Cross <sean@xobs.io>
2017-07-17 18:20:17 +0800

    Saving file

---
+++ LTC-program.ino
@@ -0,0 +1,21 @@
+// Love to Code
+
+// Getting started:
+//
+// 1. Plug your Chibi Chip into the headphone jack, and a USB port for power
+// 2. Crank the headphone volume up to max
+// 3. Press the "PROG" button near the Chibi Chip connector for one second
+//      --> The "PROG" light will turn red to indicate readiness for upload
+// 4. Pick an example from the menu above
+// 5. Hit Upload!
+
+// Don't have a Chibi Chip? Visit https://chibitronics.com/shop/ and get one now!
+extern uint32_t var;
+
+void setup() {
+  var++;
+}
+
+void loop() {
+   ;
+}
