73ed07364703c68bd1dd6aa486af6db1983a85d9
Sean Cross <sean@xobs.io>
2017-07-18 15:48:42 +0800

    Saving file

--- LTC-program.ino
+++ LTC-program.ino
@@ -1,21 +1,45 @@
-// Love to Code
+#include "Adafruit_NeoPixel.h"
 
-// Getting started:
-//
-// 1. Plug your Chibi Chip into the headphone jack, and a USB port for power
-// 2. Crank the headphone volume up to max
-// 3. Press the "PROG" button near the Chibi Chip connector for one second
-//      --> The "PROG" light will turn red to indicate readiness for upload
-// 4. Pick an example from the menu above
-// 5. Hit Upload!
+#define pixelCount 5 // number of pixels in the chain; doesn't hurt to have less actual pixels in 
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(pixelCount, LED_BUILTIN_RGB,
+                                            NEO_GRB + NEO_KHZ800);
 
-// Don't have a Chibi Chip? Visit https://chibitronics.com/shop/ and get one now!
-extern uint32_t var;
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+  WheelPos = 255 - WheelPos;
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  }
+  if(WheelPos < 170) {
+    WheelPos -= 85;
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
+}
 
 void setup() {
-  ;
+  strip.begin();
+  strip.show(); // Initialize all pixels to 'off'
 }
 
 void loop() {
-   ;
+  rainbowCycle(20);
 }
+
+// Code based on Adafruit Neopixel examples
